e5053b6d973723bdafe6ed94679bf73db8d0b0fb
Sevag Hanssian <sevagh@protonmail.com>
2021-02-13 09:51:59 -0500

    Improve directory structure

---
+++ matlab/data/prepare_data.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+import os
+import numpy
+import subprocess
+from essentia.standard import MonoLoader
+import soundfile
+
+vocal_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data-vocal')
+novocal_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data-hpss')
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog="prepare_data",
+        description="Prepare evaluation datasets for HPSS from instrument stems",
+    )
+    parser.add_argument(
+        "--sample-rate",
+        type=int,
+        default=44100,
+        help="sample rate (default: 44100 Hz)",
+    )
+    parser.add_argument(
+        "stem_dirs", nargs="+", help="directories containing instrument stems"
+    )
+    parser.add_argument(
+        "--vocals", action="store_true", help="include vocals in the mix"
+    )
+    parser.add_argument(
+        "--track-limit", type=int, default=-1, help="limit to n tracks"
+    )
+    parser.add_argument(
+        "--segment-limit", type=int, default=sys.maxsize, help="limit to n segments per track"
+    )
+    parser.add_argument(
+        "--segment-offset", type=int, default=0, help="offset of segment to start from (useful to skip intros)"
+    )
+    parser.add_argument(
+        "--segment-size", type=float, default=30.0, help="segment size in seconds"
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    data_dir = None
+    if args.vocals:
+        data_dir = vocal_dir
+    else:
+        data_dir = novocal_dir
+
+    if not os.path.isdir(data_dir):
+        os.mkdir(data_dir)
+
+    seq = 0
+    for sd in args.stem_dirs:
+        for song in os.scandir(sd):
+            for dir_name, _, file_list in os.walk(song):
+                instruments = [
+                    os.path.join(dir_name, f) for f in file_list if f.endswith(".wav")
+                ]
+                if instruments:
+                    print("Found directory containing wav files: %d" % seq)
+                    print(os.path.basename(dir_name).replace(" ", "_"))
+                    loaded_wavs = [None] * len(instruments)
+                    drum_track_index = -1
+                    vocal_track_index = -1
+                    mix_track_index = -1
+                    for i, instrument in enumerate(instruments):
+                        if "drum" in instrument.lower():
+                            drum_track_index = i
+                        elif "vocal" in instrument.lower():
+                            vocal_track_index = i
+                        elif "mix" in instrument.lower():
+                            mix_track_index = i
+
+                        # automatically resamples for us
+                        loaded_wavs[i] = MonoLoader(
+                            filename=instrument, sampleRate=args.sample_rate
+                        )()
+                    track_len = len(loaded_wavs[0])
+
+                    # ensure all stems have the same length
+                    assert (
+                        len(loaded_wavs[i]) == track_len
+                        for i in range(1, len(loaded_wavs))
+                    )
+
+                    # first create the full mix
+                    harmonic_mix = sum([l for i, l in enumerate(loaded_wavs) if i not in [drum_track_index, vocal_track_index, mix_track_index]])
+
+                    full_mix = None
+                    if args.vocals:
+                        full_mix = harmonic_mix + loaded_wavs[drum_track_index] + loaded_wavs[vocal_track_index]
+                    else:
+                        full_mix = harmonic_mix + loaded_wavs[drum_track_index]
+
+                    seg_samples = int(numpy.floor(args.segment_size*args.sample_rate))
+                    total_segs = int(numpy.floor(track_len/seg_samples))
+
+                    seg_limit = min(total_segs-1, args.segment_limit)
+
+                    for seg in range(seg_limit):
+                        if seg < args.segment_offset:
+                            continue
+                        seqstr = "%03d%04d" % (seq, seg)
+
+                        left = seg*seg_samples
+                        right = (seg+1)*seg_samples
+
+                        harm_path = os.path.join(data_dir, "{0}_harmonic.wav".format(seqstr))
+                        mix_path = os.path.join(data_dir, "{0}_mix.wav".format(seqstr))
+                        perc_path = os.path.join(data_dir, "{0}_percussive.wav".format(seqstr))
+                        vocal_path = os.path.join(data_dir, "{0}_vocal.wav".format(seqstr))
+
+                        soundfile.write(harm_path, harmonic_mix[left:right], args.sample_rate)
+                        soundfile.write(mix_path, full_mix[left:right], args.sample_rate)
+
+                        # write the drum track
+                        soundfile.write(
+                            perc_path,
+                            loaded_wavs[drum_track_index][left:right],
+                            args.sample_rate,
+                        )
+
+                        if args.vocals:
+                            # write the vocal track
+                            soundfile.write(
+                                vocal_path,
+                                loaded_wavs[vocal_track_index][left:right],
+                                args.sample_rate,
+                            )
+
+                    seq += 1
+
+                    if args.track_limit > -1:
+                        if seq == args.track_limit:
+                            return 0
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ matlab/evaluation/eval_vocal.m
@@ -0,0 +1,169 @@
+function eval_vocal(contender)
+[mypath, ~, ~] = fileparts(matlab.desktop.editor.getActiveFilename);
+addpath(genpath(fullfile(mypath, '/../../vendor/PEASS-Software-v2.0.1')));
+addpath(fullfile(mypath, '../algorithms'));
+addpath(fullfile(mypath, './contenders'));
+
+files = dir(fullfile(mypath, '../data/data-vocal/*.wav'));
+resultsDir = fullfile(mypath, './results-vocal');
+
+if not(isfolder(resultsDir))
+    mkdir(resultsDir)
+end
+
+display(contender);
+run(contender); % load testCases from contender file
+
+display(size(testCases))
+if size(testCases, 2) < size(testCases, 1)
+    testCases = testCases';
+end
+display(size(testCases))
+
+resultSize = floor(size(files, 1)/4);
+results = zeros(size(testCases, 2), resultSize, 36);
+
+options.destDir = '/tmp/';
+options.segmentationFactor = 1;
+
+findex = 1;
+
+for file = files'
+    fname = sprintf('%s/%s', file.folder, file.name);
+    
+    if contains(fname, "mix")
+        display(fname)
+        
+        % then evaluate it
+        splt = split(file.name,"_");
+        prefix = splt{1};
+        
+        harmOriginalFiles = {...
+            sprintf('%s/%s_harmonic.wav', file.folder, prefix);...
+            sprintf('%s/%s_percussive.wav', file.folder, prefix);...
+            sprintf('%s/%s_vocal.wav', file.folder, prefix)};
+
+        percOriginalFiles = {...
+            sprintf('%s/%s_percussive.wav', file.folder, prefix);...
+            sprintf('%s/%s_harmonic.wav', file.folder, prefix);...
+            sprintf('%s/%s_vocal.wav', file.folder, prefix)};
+        
+        vocalOriginalFiles = {
+            sprintf('%s/%s_vocal.wav', file.folder, prefix);...
+            sprintf('%s/%s_harmonic.wav', file.folder, prefix);...
+            sprintf('%s/%s_percussive.wav', file.folder, prefix)};
+        
+        for testcase = 1:size(testCases, 2)
+            tname = testCases{testcase}{1};
+            tfunc = testCases{testcase}{2};
+            
+            destloc = sprintf('%s/%s', resultsDir, tname);
+            
+            if not(isfolder(destloc))
+                mkdir(destloc)
+            end
+
+            fprintf('Executing %s\n', tname);
+            tfunc(fname, destloc); % execute the test case
+
+            fprintf('Evaluating %s\n', tname);
+
+            resH = PEASS_ObjectiveMeasure(harmOriginalFiles,...
+                sprintf('%s/%s_harmonic.wav', destloc, prefix), options);
+            resP = PEASS_ObjectiveMeasure(percOriginalFiles,...
+                sprintf('%s/%s_percussive.wav', destloc, prefix), options);
+            resV = PEASS_ObjectiveMeasure(vocalOriginalFiles,...
+                sprintf('%s/%s_vocal.wav', destloc, prefix), options);
+            
+            results(testcase, findex, 1) = resH.OPS;
+            results(testcase, findex, 2) = resH.TPS;
+            results(testcase, findex, 3) = resH.IPS;
+            results(testcase, findex, 4) = resH.APS;
+            results(testcase, findex, 5) = resH.ISR;
+            results(testcase, findex, 6) = resH.SIR;
+            results(testcase, findex, 7) = resH.SAR;
+            results(testcase, findex, 8) = resH.SDR;
+            results(testcase, findex, 9) = resH.qTarget;
+            results(testcase, findex, 10) = resH.qInterf;
+            results(testcase, findex, 11) = resH.qArtif;
+            results(testcase, findex, 12) = resH.qGlobal;
+            
+            results(testcase, findex, 13) = resP.OPS;
+            results(testcase, findex, 14) = resP.TPS;
+            results(testcase, findex, 15) = resP.IPS;
+            results(testcase, findex, 16) = resP.APS;
+            results(testcase, findex, 17) = resP.ISR;
+            results(testcase, findex, 18) = resP.SIR;
+            results(testcase, findex, 19) = resP.SAR;
+            results(testcase, findex, 20) = resP.SDR;
+            results(testcase, findex, 21) = resP.qTarget;
+            results(testcase, findex, 22) = resP.qInterf;
+            results(testcase, findex, 23) = resP.qArtif;
+            results(testcase, findex, 24) = resP.qGlobal;
+            
+            results(testcase, findex, 25) = resV.OPS;
+            results(testcase, findex, 26) = resV.TPS;
+            results(testcase, findex, 27) = resV.IPS;
+            results(testcase, findex, 28) = resV.APS;
+            results(testcase, findex, 29) = resV.ISR;
+            results(testcase, findex, 30) = resV.SIR;
+            results(testcase, findex, 31) = resV.SAR;
+            results(testcase, findex, 32) = resV.SDR;
+            results(testcase, findex, 33) = resV.qTarget;
+            results(testcase, findex, 34) = resV.qInterf;
+            results(testcase, findex, 35) = resV.qArtif;
+            results(testcase, findex, 36) = resV.qGlobal;
+        end
+        findex = findex + 1;
+    end
+end
+
+fprintf('*************************\n');
+fprintf('****  FINAL RESULTS  ****\n');
+fprintf('*************************\n');
+
+s = struct();
+
+for testcase = 1:size(testCases, 2)
+    tname = matlab.lang.makeValidName(testCases{testcase}{1});
+    
+    s.harmonic_peass.(tname).TPS = median(results(testcase, :, 2));
+    s.harmonic_peass.(tname).IPS = median(results(testcase, :, 3));
+    s.harmonic_peass.(tname).APS = median(results(testcase, :, 4));
+    
+    s.harmonic_bss.(tname).ISR = median(results(testcase, :, 5));
+    s.harmonic_bss.(tname).SIR = median(results(testcase, :, 6));
+    s.harmonic_bss.(tname).SAR = median(results(testcase, :, 7));
+    
+    s.harmonic_pemoq.(tname).qTarget = median(results(testcase, :, 9));
+    s.harmonic_pemoq.(tname).qInterf = median(results(testcase, :, 10));
+    s.harmonic_pemoq.(tname).qArtif = median(results(testcase, :, 11));
+    
+    s.percussive_peass.(tname).TPS = median(results(testcase, :, 14));
+    s.percussive_peass.(tname).IPS = median(results(testcase, :, 15));
+    s.percussive_peass.(tname).APS = median(results(testcase, :, 16));
+    
+    s.percussive_bss.(tname).ISR = median(results(testcase, :, 17));
+    s.percussive_bss.(tname).SIR = median(results(testcase, :, 18));
+    s.percussive_bss.(tname).SAR = median(results(testcase, :, 19));
+    
+    s.percussive_pemoq.(tname).qTarget = median(results(testcase, :, 21));
+    s.percussive_pemoq.(tname).qInterf = median(results(testcase, :, 22));
+    s.percussive_pemoq.(tname).qArtif = median(results(testcase, :, 23));
+    
+    s.vocal_peass.(tname).TPS = median(results(testcase, :, 26));
+    s.vocal_peass.(tname).IPS = median(results(testcase, :, 27));
+    s.vocal_peass.(tname).APS = median(results(testcase, :, 28));
+    
+    s.vocal_bss.(tname).ISR = median(results(testcase, :, 29));
+    s.vocal_bss.(tname).SIR = median(results(testcase, :, 30));
+    s.vocal_bss.(tname).SAR = median(results(testcase, :, 31));
+    
+    s.vocal_pemoq.(tname).qTarget = median(results(testcase, :, 33));
+    s.vocal_pemoq.(tname).qInterf = median(results(testcase, :, 34));
+    s.vocal_pemoq.(tname).qArtif = median(results(testcase, :, 35));
+end
+
+fprintf("%s\n", jsonencode(s));
+
+end
